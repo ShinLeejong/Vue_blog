fd1e8a18b068d596ad0bc55933892ebda05cf9f1
iiy4383 <znlspf123@naver.com>
2021-06-24 18:46:27 +0900

    readme

--- README.md
+++ README.md
@@ -19,6 +19,7 @@ v-container, v-card, v-layout, v-flex, etcs & props(xs12, md6, row, wrap 등)
 Vuetify에서의 Grid System에 대하여 공부했습니다. 거의 디자인적으로 불가능이
 없어보일 만큼의 많은 color 종류들과 props가 있는 것을 알게 되었고,
 더욱 더 Vuetify를 통해 많은 것을 할 수 있을 것이란 기대감을 얻게 되었습니다.
+SelectBar와 v-btn을 만들어 Sorting할 수 있도록 아주 간단하게 만들었습니다.
 
 ## 20210623
 <pre>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -8,7 +8,7 @@
             :items="['title', 'author', 'date', 'masterpiece']"
             v-model="item"
             v-on:input="sort(`${item}`, true)"
-          ></v-select>          
+          ></v-select>
         </v-flex>
         <v-flex xs6 md1>
           <v-btn color="pink" v-on:click="sort(`${item}`, false)">
@@ -16,24 +16,32 @@
           </v-btn>
         </v-flex>
       </v-layout>
-      <v-card flat class="silver lighten-5 pa-3" v-for="stuff in stuffs" :key="stuff.title">
+      <v-card
+        flat
+        class="silver lighten-5 pa-3"
+        v-for="stuff in stuffs"
+        :key="stuff.title"
+      >
         <v-layout row wrap class="pa-3 stuffs">
           <v-flex xs12 md6>
             <div class="caption grey--text">Title</div>
-            <div>{{stuff.title}}</div>
+            <div>{{ stuff.title }}</div>
           </v-flex>
           <v-flex xs6 sm4 md2>
             <div class="caption grey--text">Author</div>
-            <div>{{stuff.author}}</div>
+            <div>{{ stuff.author }}</div>
           </v-flex>
           <v-flex xs6 sm4 md2>
             <div class="caption grey--text">Date</div>
-            <div>{{stuff.date}}</div>
+            <div>{{ stuff.date }}</div>
           </v-flex>
           <v-flex xs6 sm4 md2>
             <div>
-              <v-chip small :color="`${stuff.masterpiece === '걸작' ? 'blue' : 'pink'}`">
-                {{stuff.masterpiece}}
+              <v-chip
+                small
+                :color="`${stuff.masterpiece === '걸작' ? 'blue' : 'pink'}`"
+              >
+                {{ stuff.masterpiece }}
               </v-chip>
             </div>
           </v-flex>
@@ -47,40 +55,79 @@
 import Vue from "vue";
 
 interface ObjType {
-  item: string,
+  item: string;
   stuffs: {
-    [index: string]: string, // Index Signature
-    title: string,
-    author: string,
-    date: string,
-    masterpiece: string
-  }[]
+    [index: string]: string; // Index Signature
+    title: string;
+    author: string;
+    date: string;
+    masterpiece: string;
+  }[];
 }
 
 export default Vue.extend({
   data(): ObjType {
     return {
-      item: 'title',
+      item: "title",
       stuffs: [
-        { title: 'My First Vue Project with Vuetify', author: 'Shin Leejong', date: '2021-06-24', masterpiece: '걸작'},
-        { title: 'Vuetify is so awesome', author: 'Shin Leejong', date: '2021-06-23', masterpiece: '걸작'},
-        { title: '한글로도 뭔가 적어야지', author: '신이종', date: '2021-06-24', masterpiece: '쓰레기'},
-        { title: '미래에서 온 소년', author: '신이종', date: '2022-06-24', masterpiece: '걸작'},
-        { title: 'A Cowboy From The Past', author: 'Lee Yein', date: '2020-02-02', masterpiece: '걸작'},
-        { title: '다음의 결산정리 사항에 대하여', author: '이니뚜', date: '2020-03-04', masterpiece: '걸작'},
-        { title: '에베벨베레벨베렙ㄼㄹ', author: '이예인', date: '2020-04-03', masterpiece: '쓰레기'},
-        { title: 'A stock for the purpose of long-term investment', author: 'Lee Niddo', date: '1997-03-29', masterpiece: '쓰레기'},
-      ]
-    }
+        {
+          title: "My First Vue Project with Vuetify",
+          author: "Shin Leejong",
+          date: "2021-06-24",
+          masterpiece: "걸작",
+        },
+        {
+          title: "Vuetify is so awesome",
+          author: "Shin Leejong",
+          date: "2021-06-23",
+          masterpiece: "걸작",
+        },
+        {
+          title: "한글로도 뭔가 적어야지",
+          author: "신이종",
+          date: "2021-06-24",
+          masterpiece: "쓰레기",
+        },
+        {
+          title: "미래에서 온 소년",
+          author: "신이종",
+          date: "2022-06-24",
+          masterpiece: "걸작",
+        },
+        {
+          title: "A Cowboy From The Past",
+          author: "Lee Yein",
+          date: "2020-02-02",
+          masterpiece: "걸작",
+        },
+        {
+          title: "다음의 결산정리 사항에 대하여",
+          author: "이니뚜",
+          date: "2020-03-04",
+          masterpiece: "걸작",
+        },
+        {
+          title: "에베벨베레벨베렙ㄼㄹ",
+          author: "이예인",
+          date: "2020-04-03",
+          masterpiece: "쓰레기",
+        },
+        {
+          title: "A stock for the purpose of long-term investment",
+          author: "Lee Niddo",
+          date: "1997-03-29",
+          masterpiece: "쓰레기",
+        },
+      ],
+    };
   },
   components: {},
   methods: {
-    sort(selection: string, reversion: boolean){
-      if(reversion === true)
-        this.stuffs.sort((a, b) => a[selection] < b[selection] ? 1 : -1);
-      else
-        this.stuffs.sort((a, b) => a[selection] < b[selection] ? -1 : 1);
-    }
-  }
+    sort(selection: string, reversion: boolean) {
+      if (reversion === true)
+        this.stuffs.sort((a, b) => (a[selection] < b[selection] ? 1 : -1));
+      else this.stuffs.sort((a, b) => (a[selection] < b[selection] ? -1 : 1));
+    },
+  },
 });
 </script>

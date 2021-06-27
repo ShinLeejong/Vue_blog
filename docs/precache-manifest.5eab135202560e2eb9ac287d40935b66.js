ea03dadc4383ecaeba0a712a7e55c6e869f28264
iiy4383 <znlspf123@naver.com>
2021-06-27 17:44:45 +0900

    github workflow deleted

---
+++ docs/precache-manifest.5eab135202560e2eb9ac287d40935b66.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9743da9505610ae0269",
+    "url": "/Vue_blog/css/app.de0d237d.css"
+  },
+  {
+    "revision": "f7c6c8e4c202c911996d",
+    "url": "/Vue_blog/css/chunk-08444f64.1e4b9ae7.css"
+  },
+  {
+    "revision": "40998d2c1ddcb5602cbe",
+    "url": "/Vue_blog/css/chunk-3b7e76e1.606a46d3.css"
+  },
+  {
+    "revision": "2d13066fd21f5b7dba7d",
+    "url": "/Vue_blog/css/chunk-3c4ecf62.1dad1e49.css"
+  },
+  {
+    "revision": "6b9932aa96f3a7ee1304",
+    "url": "/Vue_blog/css/chunk-vendors.b4f57a9d.css"
+  },
+  {
+    "revision": "7c5049603ff9f58b771f313e8d886593",
+    "url": "/Vue_blog/img/Lee.7c504960.jpg"
+  },
+  {
+    "revision": "20a030358832fd666b613ee1e672c756",
+    "url": "/Vue_blog/index.html"
+  },
+  {
+    "revision": "d9743da9505610ae0269",
+    "url": "/Vue_blog/js/app.6e4b8fc2.js"
+  },
+  {
+    "revision": "f7c6c8e4c202c911996d",
+    "url": "/Vue_blog/js/chunk-08444f64.d4581ac3.js"
+  },
+  {
+    "revision": "40998d2c1ddcb5602cbe",
+    "url": "/Vue_blog/js/chunk-3b7e76e1.49adc64b.js"
+  },
+  {
+    "revision": "2d13066fd21f5b7dba7d",
+    "url": "/Vue_blog/js/chunk-3c4ecf62.32449ae0.js"
+  },
+  {
+    "revision": "6b9932aa96f3a7ee1304",
+    "url": "/Vue_blog/js/chunk-vendors.1c182eb4.js"
+  },
+  {
+    "revision": "dfd3d0f4c771777e37b7aff16b61030f",
+    "url": "/Vue_blog/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Vue_blog/robots.txt"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Vue_blog/precache-manifest.46034580a92bc14808093f95d8f4fbb7.js"
+  "/Vue_blog/precache-manifest.5eab135202560e2eb9ac287d40935b66.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue_blog"});

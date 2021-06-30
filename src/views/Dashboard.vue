579a3cccf18e4ba8b89c97d4f1cd67747d73797f
iiy4383 <znlspf123@naver.com>
2021-06-30 15:21:43 +0900

    Start to work Dashboard.vue

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -12,6 +12,65 @@
           width="100%"
         ></v-responsive>
       </template> -->
+      <v-col class="mx-12 my-12">
+        <v-card class="mb-8">
+          <v-card-title class="mx-2 mb-2">
+            Teams
+          </v-card-title>
+          <v-divider class="mx-4"></v-divider>
+          <div class="v-list-item-group" role="listbox">
+            <div 
+            v-for="team in teams" 
+            :key="team.nickname"
+            role="listitem"
+            class="v-list-item"
+             >
+
+            </div>
+          </div>
+        </v-card>
+        <v-card>
+          <v-card-title class="mx-2 mb-2">
+            Board
+          </v-card-title>
+          <v-divider class="mx-4"></v-divider>
+          <div class="v-list-item-group">
+            <div 
+            v-for="notice in notices" 
+            :key="notice.id"
+            role="listitem"
+            class="v-list-item"
+             >
+            
+            </div>
+          </div>
+        </v-card>
+      </v-col>
+      <v-col class="mx-6 my-12">
+        <v-card class="mb-8">
+          <v-card-title class="mx-2 mb-2">
+            Notice
+          </v-card-title>
+          <v-divider class="mx-4"></v-divider>
+          <div class="v-list-item-group">
+            <div 
+            v-for="board in boards" 
+            :key="board.id"
+            role="listitem"
+            class="v-list-item"
+             >
+            
+            </div>
+          </div>
+        </v-card>
+        <v-card>
+          <v-card-title class="mx-2 mb-2">
+            어떻게 쓸 지 고민중인 부분
+          </v-card-title>
+          <v-divider class="mx-4"></v-divider>
+          
+        </v-card>
+      </v-col>
     </v-row>
   </v-container>
 </template>
@@ -20,11 +79,16 @@ import Lee from "./Lee.vue"
 export default {
   data() {
     return {
-      
+      teams: [],
+      notices: [],
+      boards: []
     }
   },
   components: {
     Lee
+  },
+  created() {
+    console.log(true);
   }
 };
 </script>
